0323ddfaf1599a0ef314336f85c03e863b9b85b4
HrvojeBumber <hrvoje.bumber@hotmail.com>
2014-11-27 18:09:20 -0500

    changed more of the object names

---
+++ ProjectOne/Scripts/objects/player.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../managers/asset.ts" />
+var objects;
+(function (objects) {
+    // Plane Class
+    var Player = (function () {
+        function Player(stage, game) {
+            this.stage = stage;
+            this.game = game;
+            this.image = new createjs.Sprite(managers.Assets.atlas, "plane");
+            this.image.y = 430;
+            this.width = this.image.getBounds().width;
+            this.height = this.image.getBounds().height;
+            this.image.regX = this.width / 2;
+            this.image.regY = this.height / 2;
+            game.addChild(this.image);
+            this.engineSound = createjs.Sound.play('engine', createjs.Sound.INTERRUPT_NONE, 0, 0, -1, 1, 0);
+        }
+        Player.prototype.update = function () {
+            this.image.x = this.stage.mouseX;
+        };
+        Player.prototype.destroy = function () {
+            this.engineSound.stop();
+            game.removeChild(this.image);
+        };
+        return Player;
+    })();
+    objects.Player = Player;
+})(objects || (objects = {}));
+//# sourceMappingURL=player.js.map
